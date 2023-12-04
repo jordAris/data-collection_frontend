945d0453c20f256ef8b533776ec86f1f9b508a96
jordAris <102861548+jordAris@users.noreply.github.com>
2023-12-04 20:07:12 +0100

    add my file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: "#356BB3",
+        secondary: "#FBB601",
+        third: "#FFF3CB",
+        fourth: "#6896D3",
+      },
+      fontFamily: {
+        h1: ["Lovelace Text"],
+        h2: ["Catamaran"],
+        h3: ["Montserrat"]
+      },
     },
   },
   plugins: [],
